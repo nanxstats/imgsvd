49b8b03913b429cd9555ad9eb7fa0fe5e984ba25
Yihui Xie <xie@yihui.name>
2014-02-12 01:49:23 -0600

    无空白党

--- ui.R
+++ ui.R
@@ -5,46 +5,46 @@ require('png')
 require('uuid')
 
 shinyUI(pageWithSidebar(
-  
-  headerPanel(title = 'ImgSVD - Image Compression via SVD', 
+
+  headerPanel(title = 'ImgSVD - Image Compression via SVD',
               windowTitle = 'ImgSVD - Image Compression via SVD'),
-  
+
   sidebarPanel(
-    includeCSS('boot.css'), 
-    includeHTML('header.html'), 
-    
+    includeCSS('boot.css'),
+    includeHTML('header.html'),
+
     helpText("Step 1. Upload Image"),
     fileInput('file1', 'Upload a PNG / JPEG File:'),
-    
+
     tags$hr(),
-    
+
     helpText("Step 2. Selecting k Singular Values"),
     sliderInput("intk", "Slide to choose k:", min = 1, max = 100, value = 5)
-    
+
   ),
-  
+
   mainPanel(
-    
+
     tabsetPanel(
-      
-      tabPanel("Let's do this!", 
-               h3("Original Image"), 
-               tags$hr(), 
-               tags$hr(), 
-               h3("Compressed Image"), 
-               tags$hr(), 
-      ), 
-      
-      tabPanel("Cite this app", 
+
+      tabPanel("Let's do this!",
+               h3("Original Image"),
+               tags$hr(),
                imageOutput("originImage", height = "auto"),
+               tags$hr(),
+               h3("Compressed Image"),
+               tags$hr(),
                imageOutput("svdImage", height = "auto")
+      ),
+
+      tabPanel("Cite this app",
                includeHTML('cite.html')
       )
-      
+
     ),
-    
+
     includeHTML('footer.html')
-    
+
   )
-  
+
 ))
